fbb9694339c65cf859343889636a22d0ba9fc8b2
kyoolbaba <kyoolbaba@gmail.com>
2020-07-19 22:06:56 +0530

    Added heading for Layout 1

--- src/Components/maincontainer/Layout1.js
+++ src/Components/maincontainer/Layout1.js
@@ -4,7 +4,10 @@ class Layout1 extends Component {
     render(){
         return(
             <div className="Layout1" >
-            
+            <div className="Layout1-Heading">
+            <h1>Pricing</h1>    
+            <p></p>        
+            </div>
             </div>
         )
     }
